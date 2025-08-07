29fabe5180f8581d364304c16ef825539cc3e6db
oussamasine07 <sineoussama@gmail.com>
2025-08-07 12:00:45 +0100

    create function structure in controller author

--- src/main/java/org/bookscatalogue/bookscatalaogue/controller/AuthorController.java
+++ src/main/java/org/bookscatalogue/bookscatalaogue/controller/AuthorController.java
@@ -1,4 +1,47 @@
 package org.bookscatalogue.bookscatalaogue.controller;
 
+
+import org.bookscatalogue.bookscatalaogue.dto.validation.AuthorValidationDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/author")
 public class AuthorController {
+
+    @GetMapping
+    public ResponseEntity<?> getAllAuthors () {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAuthorById (Long id) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createNewAuthor (@RequestBody AuthorValidationDTO authorValidationDTO) {
+        return null;
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
